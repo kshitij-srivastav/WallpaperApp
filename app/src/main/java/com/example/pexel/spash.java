95aa1d8bcbead192e330b29aef61082e1c55e57c
kshitij-srivastav <kshitij.13srivastav@gmail.com>
2020-09-28 14:40:25 +0530

    First commit

---
+++ app/src/main/java/com/example/pexel/spash.java
@@ -0,0 +1,27 @@
+package com.example.pexel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class spash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent =new Intent(spash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },1500);
+    }
+}
